751139d65146c5245d2eb73128ef6cc2027b04a3
José Cleiton C. Santos <69883217+jose-cleiton@users.noreply.github.com>
2022-03-09 21:45:15 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import DadJoke from "./component/DadJoke";
+import "./styles.css";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <DadJoke />
+      </div>
+    );
+  }
+}
+
+export default App;
